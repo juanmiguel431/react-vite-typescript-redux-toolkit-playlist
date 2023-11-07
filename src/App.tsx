b509849f822bf66014f09a0f2bb43efe1701517e
juanmiguel431 <juanmiguel431@gmail.com>
2023-11-06 21:32:29 -0400

    App Reset - implementation done.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import './index.css';
 import MoviePlaylist from './components/MoviePlaylist.tsx';
 import SongPlaylist from './components/SongPlaylist.tsx';
+import { useActions } from './hooks/store/useActions.ts';
 
 function App() {
-  const handleResetClick = () => {
-    //
-  };
+
+  const { appReset } = useActions();
 
   return (
     <div className="container is-fluid">
-      <button onClick={() => handleResetClick()} className="button is-danger">
+      <button onClick={() => appReset()} className="button is-danger">
         Reset Both Playlists
       </button>
       <hr />
