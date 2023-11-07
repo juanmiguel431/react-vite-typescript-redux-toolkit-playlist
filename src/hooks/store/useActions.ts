708b7f7a0743f457634eae4ecbcfd89cbf49a565
juanmiguel431 <juanmiguel431@gmail.com>
2023-11-06 22:10:52 -0400

    memorizing useActions.

--- src/hooks/store/useActions.ts
+++ src/hooks/store/useActions.ts
@@ -1,8 +1,12 @@
 import { bindActionCreators } from 'redux';
 import { actionCreators } from '../../store';
 import { useAppDispatch } from './useAppDispatch.ts';
+import { useMemo } from 'react';
 
 export const useActions = () => {
   const dispatch = useAppDispatch();
-  return bindActionCreators(actionCreators, dispatch);
+
+  return useMemo(() => {
+    return bindActionCreators(actionCreators, dispatch);
+  }, [dispatch]);
 };
